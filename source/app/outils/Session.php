7b112493ac2d676eb7c94ebb65d40635bce6c16c
Axel Michaud <axelbmichaud@gmail.com>
2019-02-02 06:23:39 -0500

    Update Session.php

--- source/app/outils/Session.php
+++ source/app/outils/Session.php
@@ -2,6 +2,17 @@
 
 namespace app\outils;
 
+use \app\modeles\Utilisateur as Utilisateur;
+
+
 class Session extends \core\Session {
-    
+
+    static public function getUtilisateur() : ?Utilisateur {
+        // if (...)
+    } 
+
+    static public function setUtilisateur(Utilisateur $user) {
+        // $_SESSION["user"]...
+    }
+
 }
