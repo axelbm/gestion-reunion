5db0b1b7f0524fd6892a04014c1c5bac94c21a68
Hugues <h-ugues@hotmail.com>
2019-02-05 16:28:23 -0500

    Connexion

---
+++ source/app/vues/connexion.php
@@ -0,0 +1,80 @@
+<div class="bg-dark">
+   <div class="container mt-4">
+        <div class="row">
+            <div class="col-lg-4 offset-lg-4" id="alert">
+                <strong id="result">Salut les copains</strong>
+            </div>
+        </div>
+   </div>
+   <div class="col-lg-4 offset-lg-4 bg-light rounded" id="login-box">
+        <h2 class="text-center mt-2">Se connecter</h2>
+        <form action="" method="post" role="form" class="p-2" id="login-frm">
+            <div class="form-group">
+            <input type="text" name="email" class="form-control" placeholder="Courrier électronique" required>
+            </div>
+            <div class="form-group">
+            <input type="password" name="password" class="form-control" placeholder="Mot de passe" required>
+            </div>
+            <div class="form-group">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" name="rem" class="custom-control-input" id="customCheck">
+                    <label for="customCheck" class="custom-control-label">Se souvenir de moi</label>
+                    <a href="#" id="forgot-btn" class="float-right">Oublié votre mot de passe ?</a>
+                </div>
+            </div>
+            <div class="form-group">
+                <input type="submit" name="login" id="login" value="Login" class="btn btn-primary btn-block">
+            </div>
+            <div class="form-group">
+                <p class="text-center">Nouvel utilisateur ? <a href="#" id="register-btn">S'inscrire ici</a></p>
+            </div>
+        </form>
+   </div>
+   <div class="row">
+            <div class="col-lg-4 offset-lg-4" id="alert">
+                <strong id="result">Salut les copains</strong>
+            </div>
+        </div>
+   </div>
+
+   <div class="col-lg-4 offset-lg-4 bg-light rounded" id="register-box">
+        <h2 class="text-center mt-2">S'inscrire</h2>
+        <form action="" method="post" role="form" class="p-2" id="register-frm">
+            <div class="form-group">
+            <input type="text" name="name" class="form-control" placeholder="Prénom" required>
+            </div>
+            <div class="form-group">
+            <input type="text" name="name" class="form-control" placeholder="Nom" required>
+            </div>
+            <div class="form-group">
+            <input type="text" name="email" class="form-control" placeholder="Courrier électronique" required>
+            </div>
+            <div class="form-group">
+            <input type="password" name="pass" class="form-control" placeholder="Mot de passe" required>
+            </div>
+            <div class="form-group">
+            <input type="password" name="cpass" class="form-control" placeholder="Veuillez confirmer votre mot de passe" required>
+            </div>
+            <div class="form-group">
+            <input type="text" name="name" class="form-control" placeholder="*No. d'Invitation" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" name="register" id="register" value="Register" class="btn btn-primary btn-block">
+            </div>
+            <div class="form-group">
+                <p class="text-center">Déjà inscrit ? <a href="#" id="login-btn">Se connecter ici</a></p>
+            </div>
+        </form>
+   </div>
+   <script type="text/javascript">
+       $(document).ready(function(){
+           $("#register-btn").click(function(){
+               $("#register-box").show();
+               $("#login-box").hide();
+           });
+           $("#login-btn").click(function(){
+               $("#register-box").hide();
+               $("#login-box").show();
+       });
+   </script>
+</div>
--- source/app/vues/dispositions/index.php
+++ source/app/vues/dispositions/index.php
@@ -4,13 +4,12 @@
 		<meta charset="utf-8" />
 		
 		<title><?= $titre ?></title>
-		
+		<!-- Axel est le meilleur -->
 
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+		<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 		
 		
 		<link rel="stylesheet" href="public/main.css">
@@ -31,5 +30,6 @@
 		<?php require "morceaux/pied de page.php"; ?>
 	</body>
 </html>
+
 
 
