c9cf70b5c70ca68e08f715bbd0a75efbbdb9db30
Axel Michaud <axelbmichaud@gmail.com>
2019-02-11 08:34:00 -0500

    Ajustement de l'accueil et optimisation de l'image
    l'image fait maintenant environ 150ko a lieu de 3000ko :)
    (c'est plus vite!)

--- source/app/vues/accueil.php
+++ source/app/vues/accueil.php
@@ -1,9 +1,9 @@
 <div>
-    <h5 class="text-center mt-2">À PROPOS D'AFH</h5>
+    <h4 class="text-center mt-2">À propos de ReunAFH</h4>
     <br>
-    <h1 style="font-size:4vw" class="text-center mt-2">Qui nous sommes</h1>
+    <h1 class="text-center mt-2 display-3">Qui nous sommes</h1>
     <br>
-    <h3 class="text-center mt-2">AFH vous aide à planifier vos réunions et vos dossiers avec </h3> 
-    <h3 class="text-center mt-2"> simplicité tout en assurant une fiabilité inégalée depuis 1950.</h3>
+    <h4 class="text-center mt-2">AFH vous aide à planifier vos réunions et vos dossiers avec </h4> 
+    <h4 class="text-center mt-2"> simplicité tout en assurant une fiabilité inégalée depuis 1950.</h3>
     <br>
 </div>
