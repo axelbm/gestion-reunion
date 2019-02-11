0d89c8ef99d75fc2c11a20f2905e9e0e517c568d
Fdazer <fdazer@gmail.com>
2019-02-10 20:33:57 -0500

    Update Reunion.php

--- source/app/dao/Reunion.php
+++ source/app/dao/Reunion.php
@@ -18,4 +18,26 @@ class Reunion extends DAO {
     public function recherche(datetime $date) : array{
         return $this->select("WHERE date = $date");
     }
+
+    public function getListe(?string $courriel, int $page, ?int $npp = 10) : array{
+        if ($courriel != null){
+            $select = "INNER JOIN participations
+                ON reunions.reunionid = participations.reunionid
+                WHERE participations.courriel = $courriel;";
+        }
+        $select += "LIMIT $page, $npp ORDER BY date";
+
+        return $this->select($select);
+    }
+
+    public function getListeParDate(?string $courriel, int $page, ?int $npp = 10) : array{
+        if ($courriel != null){
+            $select = "INNER JOIN participations
+                ON reunions.reunionid = participations.reunionid
+                WHERE participations.courriel = $courriel;";
+        }
+        $select += "WHERE date > curdate() LIMIT $page, $npp ORDER BY date";
+
+        return $this->select($select);
+    }
 }
