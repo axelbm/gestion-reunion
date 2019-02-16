61b369a61f0fe24c0235d8c8449b01305463228f
Fdazer <fdazer@gmail.com>
2019-02-16 15:45:18 -0500

    Cree controleur DetailsDossier

--- source/app/controleurs/DetailsReunion.php
+++ source/app/controleurs/DetailsReunion.php
@@ -7,7 +7,7 @@ class DetailsReunion extends \core\Controleur {
 	use atraits\Utilisateur;
 
 	public function action(array $args) : ?\Exception {
-		if (count($args) > 1)
+		if (count($args) > 0)
 			return new \Exception("erreur 404", 404);
 
 		$vue = $this->genererVue("detailsReunion");
@@ -18,18 +18,18 @@ class DetailsReunion extends \core\Controleur {
 			\core\MainControleur::rediriger("connexion");
 
         $reunion = DAO::Reunion()->find($_GET["reunion"]);
-       if (!$reunion) {
-           return new \Exception("erreur 404", 404);
-       }
-       $participations = array();
-       $pointdordres = array();
+       	if (!$reunion) {
+           	return new \Exception("erreur 404", 404);
+       	}
+       	$participations = array();
+       	$pointdordres = array();
 		
 		$utilisateurs = [];
 		foreach ($participations as $participation) {
 			$utilisateurs[$participation->getId()] = $participation->getUtilisateur();
-       }
+       	}
        
-       $dossiers = [];
+       	$dossiers = [];
 		foreach ($pointdordres as $pointdordre) {
 			$dossiers[$pointdordre->getId()] = $pointdordre->getDossier();
 		}
