537228ef3cc77c6fae179d75155598ceea861329
Axel Michaud <axelbmichaud@gmail.com>
2019-02-16 12:41:27 -0500

    Modification de la Navbar

--- source/app/vues/dispositions/morceaux/navbar.php
+++ source/app/vues/dispositions/morceaux/navbar.php
@@ -6,23 +6,31 @@
 			<li class="nav-item">
 				<a class="nav-link text-light" href="<?=WEBROOT?>">Accueil</a>
 			</li>
+
+			<?php if (isset($utilisateur)): ?>
+				<li class="nav-item">
+					<a class="nav-link text-light" href="<?=WEBROOT?>calendrier">Réunions</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link text-light" href="<?=WEBROOT?>dossiers">Dossiers</a>
+				</li>
+			<?php endif ?>
+			
 			<li class="nav-item">
-				<a class="nav-link text-light" href="<?=WEBROOT?>calendrier">Réunions</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link text-light" href="<?=WEBROOT?>dossiers">Dossiers</a>
+				<a class="nav-link text-light" href="<?=WEBROOT?>nouscontacter">Nous contacter</a>
 			</li>
 		</ul>
 
 		<?php if (!isset($erreur)): ?>
 			<?php if (isset($utilisateur)): ?>
 				<div class="dropdown">
-					<a class="dropdown-toggle text-light" data-toggle="dropdown">
+					<a class="nav-link dropdown-toggle text-light" href="#" data-toggle="dropdown">
 						<?=$utilisateur->getNomComplet();?>
 					</a>
 					<div class="dropdown-menu">
-						<a class="dropdown-item" href="#">Link 1</a>
-						<a class="dropdown-item" href="#">Link 2</a>
+						<a class="dropdown-item" href="#">Mon profil</a>
+						<a class="dropdown-item" href="#">Paramètres</a>
+						<div class="dropdown-divider"></div>
 						<a class="dropdown-item" href="<?=WEBROOT?>deconnexion">Se déconnecter</a>
 					</div>
 				</div>
