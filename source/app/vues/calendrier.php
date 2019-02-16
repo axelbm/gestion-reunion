32875c633b13a90dcd9f9dcc84642f20b7d5816e
Axel Michaud <axelbmichaud@gmail.com>
2019-02-15 23:28:50 -0500

    Ajustement des vues plus correctif
    Il manquait la fermeture du premier div dans le vue

--- source/app/vues/calendrier.php
+++ source/app/vues/calendrier.php
@@ -1,26 +1,28 @@
 
 <div class="container">
-    <h1 align="center"> - Invitation(s) - </h1> 
-    <br>
-   <?php if ($estadmin){ ?> <h6 align="right"> * Administrateur </h6>
-    <a href="reunionCreee" button type="button" class="btn btn-outline-dark float-right">Mes Réunions</a><br>
-   <?php }?>
-    <br>
-  <div class="card">
-    <div class="card-body">
-      <h4 class="card-title">15 Février 2019 - 11:00</h4>
-      <p class="card-text">#36 - Par (créateur de la réunion)</p>
-      <a href="#" class="card-link">Consulter</a>
-      <a href="#" class="card-link">Modifier</a>
-      <a href="#" class="card-link">Supprimer</a>
-      <br><span class="badge badge-success">Présent</span>
-    </div>
-  </div>
-<br>
+	<h1 align="center"> - Invitation(s) - </h1> 
+	<br>
+	<?php if ($estadmin):?> 
+		<h6 align="right"> * Administrateur </h6>
+		<a href="reunionCreee" button type="button" class="btn btn-outline-dark float-right">Mes Réunions</a><br>
+	<?php endif?>
+	<br>
+	<div class="card">
+		<div class="card-body">
+			<h4 class="card-title">15 Février 2019 - 11:00</h4>
+			<p class="card-text">#36 - Par (créateur de la réunion)</p>
+			<a href="#" class="card-link">Consulter</a>
+			<a href="#" class="card-link">Modifier</a>
+			<a href="#" class="card-link">Supprimer</a>
+			<br><span class="badge badge-success">Présent</span>
+		</div>
+	</div>
+	<br>
+</div>
 
-      <br><span class="badge badge-warning">Hésitant</span>
-      <br><span class="badge badge-danger">Absent</span>
-      <br><span class="badge badge-dark">Terminée</span>
-      <br><span class="badge badge-info">En attente</span>
+<br><span class="badge badge-warning">Hésitant</span>
+<br><span class="badge badge-danger">Absent</span>
+<br><span class="badge badge-dark">Terminée</span>
+<br><span class="badge badge-info">En attente</span>
 
 
--- source/app/vues/reunionCreee.php
+++ source/app/vues/reunionCreee.php
@@ -1,20 +1,20 @@
 <div class="container">
-    <h1 align="center"> - Mes Réunions - </h1> 
-    <br>
-    <h6 align="right"> * Administrateur </h6>
+	<h1 align="center"> - Mes Réunions - </h1> 
+	<br>
+	<h6 align="right"> * Administrateur </h6>
 
-    <a href="calendrier" button type="button" class="btn btn-outline-dark float-right">Revenir à mes invitations</a>
-    <a href="formReunion" button type="button" class="btn btn-dark float-right">Ajouter une Réunion</a><br><br>
-    <div class="card">
+	<a href="calendrier" button type="button" class="btn btn-outline-dark float-right">Revenir à mes invitations</a>
+	<a href="formReunion" button type="button" class="btn btn-dark float-right">Ajouter une Réunion</a><br><br>
+	<div class="card">
 
-    <div class="card-body">
-      <h4 class="card-title">15 Février 2019 - 11:00</h4>
-      <p class="card-text">#36 - Par (créateur de la réunion)</p>
-      <a href="#" class="card-link">Consulter</a>
-      <a href="#" class="card-link">Modifier</a>
-      <a href="#" class="card-link">Supprimer</a>
-      <br><span class="badge badge-success">Présent</span>
-    </div>
-  </div>
-
-  
+		<div class="card-body">
+			<h4 class="card-title">15 Février 2019 - 11:00</h4>
+			<p class="card-text">#36 - Par (créateur de la réunion)</p>
+			<a href="#" class="card-link">Consulter</a>
+			<a href="#" class="card-link">Modifier</a>
+			<a href="#" class="card-link">Supprimer</a>
+			<br><span class="badge badge-success">Présent</span>
+		</div>
+	</div>
+</div>
+	
