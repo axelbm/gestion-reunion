7fac3dc1679296698b83b3bab77b0e7839d93ae6
Fdazer <fdazer@gmail.com>
2019-02-16 21:52:06 -0500

    Correction

--- source/app/controleurs/SuperAdmin.php
+++ source/app/controleurs/SuperAdmin.php
@@ -2,14 +2,14 @@
 
 namespace app\controleurs;
 
-class Invitation extends \core\Controleur {
+class SuperAdmin extends \core\Controleur {
 	use atraits\Utilisateur;
 
 	public function action(array $args) : ?\Exception {
 		if (count($args) > 0)
 			return new \Exception("erreur 404", 404);
 
-		$vue = $this->genererVue("invitation");
+		$vue = $this->genererVue("superAdmin");
 		
 		$this->verifierUtilisateur();
 
