be2f60e69a5967e0b9f6337b66d46526a8fef413
Hugues <h-ugues@hotmail.com>
2019-02-16 21:19:58 -0500

    21:19

---
+++ source/app/vues/invitation.php
@@ -0,0 +1,11 @@
+<div class="col-lg-4 offset-lg-4 bg-light rounded" id="register-box">
+    <h2 class="text-center mt-2">Inviter un membre</h2><br>
+    <form action="" method="post" role="form" class="p-2" id="register-frm">
+<div class="form-group">
+        <input type="text" name="email" class="form-control" placeholder="Courrier électronique" required>
+</div>
+<div class="form-group">
+            <input type="submit" name="login" id="login" value="Inviter" class="btn btn-dark btn-block">
+        </div>
+</form>
+</div>
