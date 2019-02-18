f30d570538a9e9e5d7959306e8535edab012aa80
Fdazer <fdazer@gmail.com>
2019-02-17 22:09:22 -0500

    Correction

--- source/app/vues/formPoints.php
+++ source/app/vues/formPoints.php
@@ -4,13 +4,17 @@
     <h6 class="text-center mt-2">- un point d'ordre -</h6>
     <h4 class="text-center mt-2">Titre de la réunion </h4><br>
 
+
+    <?php $f = new \core\FormView("AjouterPointDordre"); ?>
     <form action="" method="post" role="form" class="p-2" id="point-frm">
+    <input type="hidden" name="formid" value="<?= $f->id ?>">
+    <input type="hidden" name="reunionid" value="<?= $reunion->getId() ?>">
       <div class="form-group">
         <input type="text" name="titre" class="form-control" placeholder="Nouveau Point d'ordre">
         <textarea class="form-control" placeholder="Description" rows="3" id="comment" name="description"></textarea>
       <select multiple class="form-control" id="sel1" name="dossierid">
         <?php foreach ($dossiers as $dossier) :?>
-        <option value="<?= $dossier->getId() ?>"><?= $dossier->getTitre() ?></option>
+        <option value="<?= $dossier->getId() ?>"><?= $dossier->getNom() ?></option>
         <?php endforeach;?>
       </select>
       </div>
