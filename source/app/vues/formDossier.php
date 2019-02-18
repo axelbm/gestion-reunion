be27652cdb9b88cc0fa733c7f7a702c11f5f9469
Hugues <h-ugues@hotmail.com>
2019-02-17 20:26:28 -0500

    form,modif

--- source/app/vues/formDossier.php
+++ source/app/vues/formDossier.php
@@ -1,4 +1,4 @@
-<div class="col-lg-4 offset-lg-4 bg-light rounded" id="dossier-box">
+<div class="col-lg-8 offset-lg-2 bg-light rounded" id="dossier-box">
     <h2 class="text-center mt-2">Nouveau Dossier</h2>
     <br>
     <?php $f = new \core\FormView("AjouterDossier"); ?>
@@ -6,14 +6,14 @@
 
         <input type="hidden" name="formid" value="<?= $f->id ?>">
         <div class="form-group">
-        <div class="form-group">
-        <input type="text" name="pass" class="form-control" placeholder="Titre" required>
-        </div>
-        <div class="form-group">
-        <input id="x" value="Editor content goes here" type="hidden" name="content">
-        <trix-editor input="x"></trix-editor>
-       </div>
-        <input type="submit" name="ajouter" id="ajouter" value="Ajouter" class="btn btn-dark btn-block"><br><br>
+            <div class="form-group">
+                <input type="text" name="pass" class="form-control" placeholder="Titre" required>
+            </div>
+            <div class="form-group">
+                 <input id="x" value="Editor content goes here" type="hidden" name="content">
+            <trix-editor input="x"></trix-editor><br>
+            <input type="submit" name="ajouter" id="ajouter" value="Ajouter" class="btn btn-dark float-right"><br><br>
+            </div>
         </div>
     </form>
 </div>
