07f26cb13953642efe8ddb71f2d5d2168d7073e6
Axel Michaud <axelbmichaud@gmail.com>
2019-02-17 22:34:11 -0500

    Update detailsPoints.php

--- source/app/vues/detailsPoints.php
+++ source/app/vues/detailsPoints.php
@@ -10,9 +10,30 @@
 <?php endif ?>
 </h4>
 
-
-<a href="#">Détails</a>
-    | <a href="#">Modifier</a> | <a href="#">Supprimer</a>
+Réunion du 
+<a href="<?= WEBROOT.'/detailsreunion/'.$reunion->getId()?>">
+    <?= $reunion->getDate()->format('d F Y - H\hi')?>
+</a>
 <hr>
 
 <p><?= $pointdordre->getDescription() ?></p>
+
+
+<hr>
+
+<h4>Compte rendu</h4>
+<br>
+
+<?php $f = $vue->newForm("modifCompteRendu"); ?>
+<form method="post">
+    <input type="hidden" name="formid" value="<?= $f->id ?>">
+
+    <div class="form-group">
+        <input id="compterendu" value="<?=$pointdordre->getCompteRendu()?>" type="hidden" name="compterendu">
+        <trix-editor input="compterendu" style="min-height:200px"></trix-editor>
+    </div>
+
+    <div class="form-group">
+        <input type="submit" name="Sauvegarder"  class="btn btn-dark"><br><br>
+    </div>
+</form>
