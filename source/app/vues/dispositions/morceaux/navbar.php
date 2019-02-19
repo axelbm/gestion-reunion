49409c1e7a63000e9d4b2a88b3d5a615f2f16660
Axel Michaud <axelbmichaud@gmail.com>
2019-02-19 00:01:33 -0500

    Ajout du logo AFH

--- source/app/vues/dispositions/morceaux/navbar.php
+++ source/app/vues/dispositions/morceaux/navbar.php
@@ -1,42 +1,48 @@
 <nav id="navbar" class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
 
 	<div class="container">
-		<!-- Links -->
-		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link text-light" href="<?=WEBROOT?>">Accueil</a>
-			</li>
-
-			<?php if (isset($utilisateur)): ?>
-				<li class="nav-item">
-					<a class="nav-link text-light" href="<?=WEBROOT?>calendrier">Réunions</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link text-light" href="<?=WEBROOT?>dossiers">Dossiers</a>
-				</li>
-			<?php endif ?>
+		<div>
+			<!-- Links -->
 			
-			
-			<li class="nav-item">
-				<a class="nav-link text-light" href="<?=WEBROOT?>nouscontacter">Nous contacter</a>
-			</li>
-
-			<?php if (isset($utilisateur) && $utilisateur->estAdministrateur()): ?>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle text-light" href="#" id="navbardrop" data-toggle="dropdown">
-						Administration
-					</a>
-					<div class="dropdown-menu">
-						<a class="dropdown-item" href="<?=WEBROOT?>invitation">Inviter</a>
-						
-						<?php if ($utilisateur->estSuperAdministrateur()): ?>
-							<a class="dropdown-item" href="<?=WEBROOT?>superAdmin">Super Admin</a>
-						<?php endif ?>
-					</div>
+			<ul class="navbar-nav">
+				<a class="nav-item" href="<?=WEBROOT?>">
+					<img src="<?= PUBLICROOT.'images/logo blanc.png'?>" alt="Logo" style="height: 30px; margin: 5px 10px 5px 0px;">
+				</a>
+				<li class="nav-item">
+					<a class="nav-link text-light" href="<?=WEBROOT?>">Accueil</a>
 				</li>
+
+				<?php if (isset($utilisateur)): ?>
+					<li class="nav-item">
+						<a class="nav-link text-light" href="<?=WEBROOT?>calendrier">Réunions</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-light" href="<?=WEBROOT?>dossiers">Dossiers</a>
+					</li>
+				<?php endif ?>
 				
-			<?php endif ?>
-		</ul>
+				
+				<li class="nav-item">
+					<a class="nav-link text-light" href="<?=WEBROOT?>nouscontacter">Nous contacter</a>
+				</li>
+
+				<?php if (isset($utilisateur) && $utilisateur->estAdministrateur()): ?>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle text-light" href="#" id="navbardrop" data-toggle="dropdown">
+							Administration
+						</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="<?=WEBROOT?>invitation">Inviter</a>
+							
+							<?php if ($utilisateur->estSuperAdministrateur()): ?>
+								<a class="dropdown-item" href="<?=WEBROOT?>superAdmin">Super Admin</a>
+							<?php endif ?>
+						</div>
+					</li>
+					
+				<?php endif ?>
+			</ul>
+		</div>
 
 		<?php if (!isset($erreur)): ?>
 			<?php if (isset($utilisateur)): ?>
