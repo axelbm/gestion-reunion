f727b6733fd6fe27389e1872b3bed1461cd3370f
Axel Michaud <axelbmichaud@gmail.com>
2019-02-18 23:53:56 -0500

    Mise a jour des statuts

--- sql/insert statut.sql
+++ sql/insert statut.sql
@@ -1,10 +1,9 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
-INSERT INTO `participationstatus` (`statutid`, `nom`, `description`) VALUES
-('Abs', 'Absent', 'La personne invitee va etre absente.'),
-('Ann', 'Annuler', 'La reunion ou l\'invitation a ete annulee.'),
-('EnAt', 'En Attente', 'En attente d\'une reponse de la personne invitee.'),
-('Hes', 'Hesitant', 'La personne invitee ne sait pas si elle va etre presente.'),
-('Pres', 'Present', 'La personne invitee va etre presente.'),
-('Term', 'Terminee', 'La reunion est terminee.');
+INSERT INTO `participationstatut` (`statutid`, `nom`, `description`) VALUES
+    ('Abs', 'Absent', 'La personne invitee va etre absente.'),
+    ('Ann', 'Annulée', 'La réunion ou l\'invitation a ete annulée.'),
+    ('EnAt', 'En Attente', 'En attente d\'une reponse de la personne invitee.'),
+    ('Hes', 'Hesitant', 'La personne invitee ne sait pas si elle va etre presente.'),
+    ('Pres', 'Present', 'La personne invitee va etre presente.'),
+    ('Term', 'Terminée', 'La réunion est terminee.');
+    ('Org', 'Organisateur', 'Organisateur de la réunion.');
